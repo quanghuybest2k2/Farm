5091f866e1b78f80ffd98c0d27b56a8a15bd0323
Doan Quang Huy <quanghuybest@gmail.com>
2024-03-09 22:35:23 +0700

    feat: get Environment sample with province data

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Environment from "../pages/Environment";
 
 const MyRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/environments" element={<Environment />} />
     </Routes>
   );
 };
