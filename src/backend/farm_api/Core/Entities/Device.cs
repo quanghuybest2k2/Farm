a733515139130b5382c30127efac35be739619d5
Hung-Alex <alexanderpham59@gmai.com>
2024-03-09 14:46:27 +0700

    init

---
+++ src/backend/farm_api/Core/Entities/Device.cs
@@ -0,0 +1,19 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Device : BaseEntity,IModifier
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Status { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
