b6d817a4f3b3e32d98c77d12a58b1df915f5813d
Doan Quang Huy <quanghuybest@gmail.com>
2024-03-12 19:56:33 +0700

    feat: add env

--- src/frontend/src/pages/Environment/EnvironmentCreate.js
+++ src/frontend/src/pages/Environment/EnvironmentCreate.js
@@ -2,19 +2,18 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import config from "../../config";
-import Loading from "../../components/Loading";
 import swal from "sweetalert";
 
 const EnvironmentCreate = () => {
   const navigate = useNavigate();
   const [inputErrorList, setInputErrorList] = useState({});
-  const [isVisibleLoading, setIsVisibleLoading] = useState(true);
 
   const [environment, setEnvironment] = useState({
-    name: "",
-    type: "",
+    temperature: 0,
+    airQuality: 0,
+    sensorLocation: "",
+    brightness: 0,
   });
-
   const handleInput = (e) => {
     e.persist();
     setEnvironment({ ...environment, [e.target.name]: e.target.value });
@@ -23,93 +22,116 @@ const EnvironmentCreate = () => {
   const saveEnvironment = (e) => {
     e.preventDefault();
 
-    setIsVisibleLoading(true);
-
     const data = {
-      name: environment.name,
-      type: environment.type,
+      airQuality: environment.airQuality,
+      temperature: environment.temperature,
+      sensorLocation: environment.sensorLocation,
+      brightness: environment.brightness,
     };
 
     axios
       .post(`${config.API_URL}/environments`, data)
       .then((res) => {
-        if (res.data) {
-          //   alert(res.data.message);
-          swal("Success", res.data.message, "success");
-          navigate("/environments");
-          setIsVisibleLoading(false);
-        }
+        //   alert(res.data.message);
+        swal("Success", "Create successfully", "success");
+        navigate("/environments");
       })
       .catch(function (error) {
         if (error.response) {
-          if (error.response.status === 422) {
+          if (error.response.status === 400) {
             setInputErrorList(error.response.data.errors);
           }
           if (error.response.status === 500) {
             // alert(error.response.data);
             swal("Oops!", error.response.data, "error");
           }
-          setIsVisibleLoading(false);
         }
       });
   };
 
   return (
     <>
-      {isVisibleLoading ? (
-        <Loading />
-      ) : (
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="card">
-                <div className="card-header">
-                  <h4>
-                    Add Environment
-                    <Link
-                      to="/environments"
-                      className="btn btn-danger float-end"
-                    >
-                      Back
-                    </Link>
-                  </h4>
-                </div>
-                <div className="card-body">
-                  <form onSubmit={saveEnvironment}>
-                    <div className="mb-3">
-                      <label>Name</label>
-                      <input
-                        type="text"
-                        name="name"
-                        value={environment.name}
-                        onChange={handleInput}
-                        className="form-control"
-                      />
-                      <span className="text-danger">{inputErrorList.name}</span>
-                    </div>
-                    <div className="mb-3">
-                      <label>Type</label>
-                      <input
-                        type="text"
-                        name="type"
-                        value={environment.type}
-                        onChange={handleInput}
-                        className="form-control"
-                      />
-                      <span className="text-danger">{inputErrorList.type}</span>
-                    </div>
-                    <div className="mb-3">
-                      <button type="submit" className="btn btn-primary">
-                        Save Environment
-                      </button>
-                    </div>
-                  </form>
-                </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-header">
+                <h4>
+                  Add Environment
+                  <Link to="/environments" className="btn btn-danger float-end">
+                    Back
+                  </Link>
+                </h4>
+              </div>
+              <div className="card-body">
+                <form onSubmit={saveEnvironment}>
+                  <div className="mb-3">
+                    <label>Temperature</label>
+                    <input
+                      type="number"
+                      name="temperature"
+                      value={environment.temperature}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">
+                      {inputErrorList.temperature}
+                    </span>
+                  </div>
+
+                  <div className="mb-3">
+                    <label>airQuality</label>
+                    <input
+                      type="number"
+                      name="airQuality"
+                      value={environment.airQuality}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">
+                      {inputErrorList.airQuality}
+                    </span>
+                  </div>
+
+                  <div className="mb-3">
+                    <label>SensorLocation</label>
+                    <input
+                      type="text"
+                      name="sensorLocation"
+                      value={environment.sensorLocation}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">
+                      {inputErrorList.sensorLocation}
+                    </span>
+                  </div>
+
+                  <div className="mb-3">
+                    <label>Brightness</label>
+                    <input
+                      type="number"
+                      name="brightness"
+                      value={environment.brightness}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">
+                      {inputErrorList.brightness}
+                    </span>
+                  </div>
+
+                  <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">
+                      Save Environment
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
