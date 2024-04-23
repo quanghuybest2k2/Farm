22add36c36b4423f1cd1048993a10df092732bc6
Đoàn Quang Huy <82820145+quanghuybest2k2@users.noreply.github.com>
2024-04-23 09:33:04 +0700

    ui auto config

--- src/frontend/src/views/AutoConfig/CreateConfig.js
+++ src/frontend/src/views/AutoConfig/CreateConfig.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   CButton,
   CCard,
@@ -10,16 +10,16 @@ import {
   CFormInput,
   CFormLabel,
   CRow,
-} from '@coreui/react'
-import { DocsExample } from 'src/components'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
-import { subDays, format, startOfDay, endOfDay } from 'date-fns'
+  CLink,
+} from "@coreui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { subDays, format, startOfDay, endOfDay } from "date-fns";
 
 const CreateConfig = () => {
-  const [startDate, setStartDate] = useState(null)
-  const [endDate, setEndDate] = useState(null)
-  const endDateNow = endOfDay(new Date())
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const endDateNow = endOfDay(new Date());
 
   return (
     <>
@@ -28,34 +28,55 @@ const CreateConfig = () => {
           <CCard className="mb-4">
             <CCardHeader>
               <strong>Create</strong> <small>New Schedules</small>
+              <CLink href="#/auto-config">
+                <CButton className="float-end" color="primary">
+                  Back
+                </CButton>
+              </CLink>
             </CCardHeader>
             <CCardBody>
               <p className="text-body-secondary small">
-                Create new automatic profiles to replace scheduled manual <code>on/off</code>.
+                Create new automatic profiles to replace scheduled manual{" "}
+                <code>on/off</code>.
               </p>
               <CForm>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Type</CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Type
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput type="text" />
+                    <CFormInput type="text" placeholder="Enter type...." />
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Area</CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Area
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput type="text" />
+                    <CFormInput type="text" placeholder="Enter area...." />
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Area Sensor</CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Area Sensor
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput type="text" />
+                    <CFormInput
+                      type="text"
+                      placeholder="Enter area sensor...."
+                    />
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Device</CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Device
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormSelect size="sm" className="mb-3" aria-label="Select device">
+                    <CFormSelect
+                      size="large"
+                      className="mb-3"
+                      aria-label="Select device"
+                    >
                       <option>Select device</option>
                       <option value="0">Quạt 1</option>
                       <option value="1">Đèn 1</option>
@@ -64,9 +85,15 @@ const CreateConfig = () => {
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Status</CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Status
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormSelect size="sm" className="mb-3" aria-label="Select status">
+                    <CFormSelect
+                      size="large"
+                      className="mb-3"
+                      aria-label="Select status"
+                    >
                       <option>Select status</option>
                       <option value="0">Off</option>
                       <option value="1">On</option>
@@ -74,15 +101,28 @@ const CreateConfig = () => {
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">From value</CFormLabel>
-                  <CCol sm={10}>
-                    <CFormInput type="number" />
-                  </CCol>
-                </CRow>
-                <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">To value</CFormLabel>
-                  <CCol sm={10}>
-                    <CFormInput type="number" />
+                  <CCol sm={5}>
+                    <div className="d-flex mt-3 align-items-center">
+                      <div className="me-2">
+                        <h6 className="mb-3">
+                          From value <code>(number)</code>
+                        </h6>
+                        <CFormInput
+                          type="number"
+                          placeholder="Enter start value...."
+                        />
+                      </div>
+                      <span className="text-muted mt-4">-</span>
+                      <div className="ms-2">
+                        <h6 className="mb-3">
+                          To value <code>(number)</code>
+                        </h6>
+                        <CFormInput
+                          type="number"
+                          placeholder="Enter end value...."
+                        />
+                      </div>
+                    </div>
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
@@ -125,26 +165,34 @@ const CreateConfig = () => {
                     </div>
                   </CCol>
                 </CRow>
-                <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">Active</CFormLabel>
+                <CRow className="mt-4">
+                  <CFormLabel className="col-sm-2 col-form-label">
+                    Active
+                  </CFormLabel>
                   <CCol sm={10}>
-                    <CFormSelect size="sm" className="mb-3" aria-label="Select active">
+                    <CFormSelect
+                      size="large"
+                      className="mb-3"
+                      aria-label="Select active"
+                    >
                       <option>Select active</option>
                       <option value="0">Off</option>
                       <option value="1">On</option>
                     </CFormSelect>
                   </CCol>
                 </CRow>
-                <CButton color="primary" type="submit">
-                  Create
-                </CButton>
+                <CCol className="mt-3">
+                  <CButton color="primary" type="submit">
+                    Create
+                  </CButton>
+                </CCol>
               </CForm>
             </CCardBody>
           </CCard>
         </CCol>
       </CRow>
     </>
-  )
-}
+  );
+};
 
-export default CreateConfig
+export default CreateConfig;
