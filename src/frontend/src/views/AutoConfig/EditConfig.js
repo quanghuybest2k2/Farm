a6b215470142080212348c3b94441e6c13537ea2
Doan Quang Huy <quanghuybest@gmail.com>
2024-04-23 12:39:45 +0700

    xóa area sensor

--- src/frontend/src/views/AutoConfig/EditConfig.js
+++ src/frontend/src/views/AutoConfig/EditConfig.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -11,15 +11,15 @@ import {
   CFormLabel,
   CRow,
   CLink,
-} from "@coreui/react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { subDays, format, startOfDay, endOfDay } from "date-fns";
+} from '@coreui/react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { subDays, format, startOfDay, endOfDay } from 'date-fns'
 
 const EditConfig = () => {
-  const [startDate, setStartDate] = useState("2024-04-08");
-  const [endDate, setEndDate] = useState(null);
-  const endDateNow = endOfDay(new Date());
+  const [startDate, setStartDate] = useState('2024-04-08')
+  const [endDate, setEndDate] = useState(null)
+  const endDateNow = endOfDay(new Date())
 
   return (
     <>
@@ -36,57 +36,25 @@ const EditConfig = () => {
             </CCardHeader>
             <CCardBody>
               <p className="text-body-secondary small">
-                Edit automatic profiles to replace scheduled manual{" "}
-                <code>on/off</code>.
+                Edit automatic profiles to replace scheduled manual <code>on/off</code>.
               </p>
               <CForm>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Type
-                  </CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">Type</CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput
-                      type="text"
-                      placeholder="Enter type...."
-                      value="Nhiệt độ"
-                    />
+                    <CFormInput type="text" placeholder="Enter type...." value="Nhiệt độ" />
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Area
-                  </CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">Area</CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput
-                      type="text"
-                      placeholder="Enter area...."
-                      value="farm-2"
-                    />
+                    <CFormInput type="text" placeholder="Enter area...." value="farm-2" />
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Area Sensor
-                  </CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">Device</CFormLabel>
                   <CCol sm={10}>
-                    <CFormInput
-                      type="text"
-                      placeholder="Enter area sensor...."
-                      value="KV2"
-                    />
-                  </CCol>
-                </CRow>
-                <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Device
-                  </CFormLabel>
-                  <CCol sm={10}>
-                    <CFormSelect
-                      size="large"
-                      className="mb-3"
-                      aria-label="Select device"
-                      value="1"
-                    >
+                    <CFormSelect size="large" className="mb-3" aria-label="Select device" value="1">
                       <option>Select device</option>
                       <option value="0">Quạt 1</option>
                       <option value="1">Đèn 1</option>
@@ -95,16 +63,9 @@ const EditConfig = () => {
                   </CCol>
                 </CRow>
                 <CRow className="mb-3">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Status
-                  </CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">Status</CFormLabel>
                   <CCol sm={10}>
-                    <CFormSelect
-                      size="large"
-                      className="mb-3"
-                      aria-label="Select status"
-                      value="1"
-                    >
+                    <CFormSelect size="large" className="mb-3" aria-label="Select status" value="1">
                       <option>Select status</option>
                       <option value="0">Off</option>
                       <option value="1">On</option>
@@ -118,22 +79,14 @@ const EditConfig = () => {
                         <h6 className="mb-3">
                           From value <code>(number)</code>
                         </h6>
-                        <CFormInput
-                          type="number"
-                          placeholder="Enter start value...."
-                          value="25"
-                        />
+                        <CFormInput type="number" placeholder="Enter start value...." value="25" />
                       </div>
                       <span className="text-muted mt-4">-</span>
                       <div className="ms-2">
                         <h6 className="mb-3">
                           To value <code>(number)</code>
                         </h6>
-                        <CFormInput
-                          type="number"
-                          placeholder="Enter end value...."
-                          value="30"
-                        />
+                        <CFormInput type="number" placeholder="Enter end value...." value="30" />
                       </div>
                     </div>
                   </CCol>
@@ -179,16 +132,9 @@ const EditConfig = () => {
                   </CCol>
                 </CRow>
                 <CRow className="mt-4">
-                  <CFormLabel className="col-sm-2 col-form-label">
-                    Active
-                  </CFormLabel>
+                  <CFormLabel className="col-sm-2 col-form-label">Active</CFormLabel>
                   <CCol sm={10}>
-                    <CFormSelect
-                      size="large"
-                      className="mb-3"
-                      aria-label="Select active"
-                      value="1"
-                    >
+                    <CFormSelect size="large" className="mb-3" aria-label="Select active" value="1">
                       <option>Select active</option>
                       <option value="0">Off</option>
                       <option value="1">On</option>
@@ -206,7 +152,7 @@ const EditConfig = () => {
         </CCol>
       </CRow>
     </>
-  );
-};
+  )
+}
 
-export default EditConfig;
+export default EditConfig
